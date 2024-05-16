9228820733b06ad3c89a015d8d393d9bf799b5fd
Louis-ThomasNolet <louis.thomas.nolet@icloud.com>
2024-05-16 17:57:59 -0400

    student card fixed need to add buttons

--- src/components/StudentCard.vue
+++ src/components/StudentCard.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
+import { useUserStore } from '@/stores/userStore'
+import { defineProps } from 'vue'
+import { onMounted } from 'vue'
 
 
-
-
+const userStore = useUserStore()
 
 const colors = ['red', 'yellow', 'green', 'god']
-function getColor(health: number) {
+
+const getColor = (health: number) => {
     if (health <= 30)
         return colors[0]
     else if (health <= 60)
@@ -16,33 +19,57 @@ function getColor(health: number) {
     else
         return colors[3]
 }
+
+const props = defineProps({
+    id: Number
+})
+
+const student = ref(null)
+onMounted(async () => {
+    await userStore.getUserById(props.id)
+    student.value = userStore.user
+})
+
 </script>
 <template>
-    <div class="student-card col-9" :class="getColor(student.health)">
+    <div class="student-card col-9" v-if="student" :class="getColor(student.health)">
         <h3>{{ student.name }}</h3>
-        <p>{{ student.email }}</p>
-        <p>{{ student.health }}</p>
+
+        <!-- ajouter bouton pour ajouter et retirer la vie -->
     </div>
 </template>
+
+
 <style>
 .student-card {
-  border: 1px solid #4aa3a3;
-  border-radius: 4px;
-  padding: 20px;
-  margin: 10px;
-  width: 200px;
+    border: 1px solid #4aa3a3;
+    border-radius: 4px;
+    padding: 20px;
+    margin: 10px;
+    width: 200px;
 }
 
-.red{
-    width: 20%; padding: 10px; background-image: url(/src/assets/red.png);
+.red {
+    width: 20%;
+    padding: 10px;
+    background-image: url(/src/assets/red.png);
 }
-.god{
-    width: 20%; padding: 10px; background-image: url(/src/assets/rainbow.gif);
+
+.god {
+    width: 20%;
+    padding: 10px;
+    background-image: url(/src/assets/rainbow.gif);
 }
-.green{
-    width: 20%; padding: 10px; background-image: url(/src/assets/green.png);
+
+.green {
+    width: 20%;
+    padding: 10px;
+    background-image: url(/src/assets/green.png);
 }
-.yellow{
-    width: 20%; padding: 10px; background-image: url(/src/assets/yellow.png);
+
+.yellow {
+    width: 20%;
+    padding: 10px;
+    background-image: url(/src/assets/yellow.png);
 }
 </style>
